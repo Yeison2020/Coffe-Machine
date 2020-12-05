db65645af1fc433a508efe0136fd13ab1fd7abc3
Yeison2020 <Yeisontech2020@gmail.com>
2020-12-04 19:41:13 -0500

    Add files via upload

---
+++ Nato alphabet/main.py
@@ -0,0 +1,17 @@
+import pandas
+
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+
+phonetic_dict = {row.letter:row.code for (index,row) in data.iterrows()}
+
+
+
+
+word = input("Enter a word:\n").upper()
+
+output = [phonetic_dict[letter] for letter in word]
+
+
+print(output)
